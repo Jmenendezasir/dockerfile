2474991530238e6689e3ea1507086c03159fb654
jmenendezasir <jmenendezasir@gmail.com>
2021-12-16 13:11:17 +0100

    16/10/2021

--- proyecto/apachephplaravel/git.sh
+++ proyecto/apachephplaravel/git.sh
@@ -11,6 +11,12 @@ git remote add origin $repoGit
 git pull origin main
 git reset --hard origin/main
 
+# Haremos un migrate en laravel tras importar el repositorio. Eliminaremos el migrate que crea por defecto Passport.
+
+rm $defaultRoute/database/migrations/2014_10_12_000000_create_users_table.php
+
+php artisan migrate
+php artisan passport:install
 
 service apache2 start
 tail -f /dev/null
